535e622479d726a64bf24a2feca0831557a7fcf1
unknown <SIG3GA@PC5022.supinfogame.local>
2015-03-19 10:45:49 +0100

    Update days system

--- Assets/script/jours.cs
+++ Assets/script/jours.cs
@@ -2,42 +2,78 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class jours : MonoBehaviour {
+public class jours : MonoBehaviour
+{
 
     private int jour;
 
     public GameObject texte;
     public GameObject garden;
 
-    private int longeurJour;
-    private int tempsActuel;
+    public GameObject personnage;
 
-	void Start () {
-        tempsActuel = 0;
-        longeurJour = 3600;
-        jour = 1;
-        texte.GetComponent<Text>().text = jour.ToString();
-	}
-	
-    public void Reset()
+    private int longeurJour;
+    private float tempsActuel;
+
+    void Start()
     {
         tempsActuel = 0;
-        longeurJour = 3600;
+        longeurJour = 60;
         jour = 1;
         texte.GetComponent<Text>().text = jour.ToString();
     }
 
-	void Update () {
-        tempsActuel++;
-        if(tempsActuel == longeurJour)
+    public void Reset()
+    {
+        tempsActuel = 0;
+        longeurJour = 60;
+        jour = 1;
+        texte.GetComponent<Text>().text = jour.ToString();
+    }
+
+    void Update()
+    {
+        tempsActuel += Time.deltaTime;
+        if (tempsActuel >= longeurJour)
         {
             jour++;
             texte.GetComponent<Text>().text = jour.ToString();
-            tempsActuel = 0;
-            player.GrainesDeco++;
-            player.GrainesMedoc++;
-            player.GrainesMusic++;
+            tempsActuel -= longeurJour;
+            if (personnage.GetComponent<personnage>().GetEpanouissement() > 60)
+            {
+                player.GrainesMedoc++;
+                int Rand;
+                for (int i = 0; i < 2; i++)
+                {
+                    Rand = (int)Random.Range(0, 2);
+                    if (Rand == 0)
+                    {
+                        player.GrainesDeco++;
+                    }
+                    else
+                    {
+                        player.GrainesMusic++;
+                    }
+                }
+            }
+            else if (personnage.GetComponent<personnage>().GetEpanouissement() > 30)
+            {
+                player.GrainesMedoc++;
+                int Rand = (int)Random.Range(0, 2);
+                if (Rand == 0)
+                {
+                    player.GrainesDeco++;
+                }
+                else
+                {
+                    player.GrainesMusic++;
+                }
+            }
+            else
+            {
+                player.GrainesMedoc++;
+            }
             garden.GetComponent<jardin>().CheckGraines();
         }
-	}
+    }
 }
